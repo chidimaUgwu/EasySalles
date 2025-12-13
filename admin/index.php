4fb9bca9dceda76797ed8c92316cbae751e125c1
Chidima Praise Jude Ugwu <chidima.ugwu@ashesi.edu.gh>
2025-12-13 14:09:42 +0000

    Update index.php

--- admin/index.php
+++ admin/index.php
@@ -4,7 +4,7 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-//session_start();
+session_start();
 
 // Check if user is logged in
 if (!isset($_SESSION['user_id'])) {
