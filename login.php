78ac6a6ff740ebd3c794e1464153dd82def343b6
Chidima Praise Jude Ugwu <chidima.ugwu@ashesi.edu.gh>
2025-12-13 11:26:34 +0000

    Update login.php

--- login.php
+++ login.php
@@ -62,7 +62,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
                 }
                 
                 if (empty($error)) {
-                    header('Location: ' . ($user['role'] == 1 ? 'admin-dashboard.php' : 'staff-dashboard.php'));
+                    header('Location: ' . ($user['role'] == 1 ? 'admin/dashboard.php' : 'staff-dashboard.php'));
                     exit();
                 }
             } else {
