ff1848d4f1f2b4ca7e4f83c48a2d1b9db6cc4394
Chidima Praise Jude Ugwu <chidima.ugwu@ashesi.edu.gh>
2025-12-13 06:49:02 +0000

    Create dashboard.php

---
+++ admin/dashboard.php
@@ -0,0 +1,14 @@
+<?php
+require 'includes/auth.php';
+require_admin();
+
+$page_title = 'Admin Dashboard';
+include 'includes/header.php';
+?>
+
+<div class="text-center">
+    <h1>Welcome, Admin!</h1>
+    <p>This is your admin dashboard. More features coming soon!</p>
+</div>
+
+<?php include 'includes/footer.php'; ?>
