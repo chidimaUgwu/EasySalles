bc7c336b5be422a5781cb8767e6b1c03f3c48464
Chidima Praise Jude Ugwu <chidima.ugwu@ashesi.edu.gh>
2025-12-14 02:02:32 +0000

    Update functions.php

--- admin/includes/functions.php
+++ admin/includes/functions.php
@@ -1,6 +1,6 @@
 <?php
 // admin/includes/functions.php
-session_start();
+//session_start();
 
 // Redirect if not logged in or not admin
 if (!isset($_SESSION['user_id']) || $_SESSION['role'] != 1) {
