0e5a4be1dc71d337262c30df9416a17313631ddb
Chidima Praise Jude Ugwu <chidima.ugwu@ashesi.edu.gh>
2025-12-17 16:19:24 +0000

    V

--- ajax/update_cart_quantity.php
+++ ajax/update_cart_quantity.php
@@ -1,4 +1,9 @@
 <?php
+// Turn on full error reporting for debugging
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 session_start();
 
 header('Content-Type: application/json');
