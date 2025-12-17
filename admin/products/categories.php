127da783cbd65020757fefd1e410e186efa72d89
Chidima Praise Jude Ugwu <chidima.ugwu@ashesi.edu.gh>
2025-12-17 23:55:29 +0000

    v

--- admin/products/categories.php
+++ admin/products/categories.php
@@ -1,4 +1,10 @@
 <?php
+
+// Turn on full error reporting for debugging
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 // admin/products/categories.php
 $page_title = "Product Categories";
 require_once '../includes/header.php';
