11517fd644a9df9674dbff9b0c1091871cf2b199
Chidima Praise Jude Ugwu <chidima.ugwu@ashesi.edu.gh>
2025-12-17 16:48:51 +0000

    V

--- ajax/remove_from_cart.php
+++ ajax/remove_from_cart.php
@@ -6,10 +6,19 @@ header('Content-Type: application/json');
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $product_id = $_POST['product_id'] ?? 0;
     
-    if (isset($_SESSION['cart'][$product_id])) {
-        unset($_SESSION['cart'][$product_id]);
+    if (isset($_SESSION['cart']['items'])) {
+        // Remove item from cart
+        $_SESSION['cart']['items'] = array_filter($_SESSION['cart']['items'], function($item) use ($product_id) {
+            return $item['product_id'] != $product_id;
+        });
+        
+        // Reset array keys
+        $_SESSION['cart']['items'] = array_values($_SESSION['cart']['items']);
+        
+        // Update total count
+        $_SESSION['cart']['count'] = array_sum(array_column($_SESSION['cart']['items'], 'quantity'));
     }
     
-    echo json_encode(['success' => true]);
+    echo json_encode(['success' => true, 'count' => $_SESSION['cart']['count'] ?? 0]);
 }
 ?>
