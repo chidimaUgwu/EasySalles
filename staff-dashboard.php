51b784817e6320ba26776ee737352d74f5aa05b9
Chidima Praise Jude Ugwu <chidima.ugwu@ashesi.edu.gh>
2025-12-17 02:04:34 +0000

    add

--- staff-dashboard.php
+++ staff-dashboard.php
@@ -1,5 +1,10 @@
 <?php
 // staff-dashboard.php
+// Turn on full error reporting for debugging
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 
 // Turn on full error reporting for debugging
 ini_set('display_errors', 1);
