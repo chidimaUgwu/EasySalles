4a97897172494c2c16d709d0ee1ffeb462ef9173
Chidima Praise Jude Ugwu <chidima.ugwu@ashesi.edu.gh>
2025-12-17 16:49:27 +0000

    V

--- ajax/update_cart_quantity.php
+++ ajax/update_cart_quantity.php
@@ -1,19 +1,22 @@
 <?php
-// Turn on full error reporting for debugging
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
-
 session_start();
 
 header('Content-Type: application/json');
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $product_id = $_POST['product_id'] ?? 0;
-    $quantity = $_POST['quantity'] ?? 1;
+    $quantity = intval($_POST['quantity'] ?? 1);
     
-    if (isset($_SESSION['cart'][$product_id])) {
-        $_SESSION['cart'][$product_id] = $quantity;
+    if (isset($_SESSION['cart']['items'])) {
+        foreach ($_SESSION['cart']['items'] as &$item) {
+            if ($item['product_id'] == $product_id) {
+                $item['quantity'] = $quantity;
+                break;
+            }
+        }
+        
+        // Update total count
+        $_SESSION['cart']['count'] = array_sum(array_column($_SESSION['cart']['items'], 'quantity'));
     }
     
     echo json_encode(['success' => true]);
