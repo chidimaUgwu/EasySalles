7c973a10957cbea2001b7eec2e1fc27535a8990c
Chidima Praise Jude Ugwu <chidima.ugwu@ashesi.edu.gh>
2025-12-17 15:44:27 +0000

    CREATE

---
+++ ajax/get_cart_count.php
@@ -0,0 +1,15 @@
+<?php
+// ajax/get_cart_count.php
+session_start();
+
+header('Content-Type: application/json');
+
+$count = 0;
+if (isset($_SESSION['cart'])) {
+    foreach ($_SESSION['cart'] as $item) {
+        $count += $item['quantity'];
+    }
+}
+
+echo json_encode(['success' => true, 'count' => $count]);
+?>
