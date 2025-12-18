6abed292da18e67625d447a7182eda43f906da54
Chidima Praise Jude Ugwu <chidima.ugwu@ashesi.edu.gh>
2025-12-18 02:30:42 +0000

    v

--- admin/schedule/index.php
+++ admin/schedule/index.php
@@ -1,4 +1,10 @@
 <?php
+
+// Turn on full error reporting for debugging
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 // admin/schedule/index.php
 $page_title = "Shift Schedule Management";
 require_once '../includes/header.php';
